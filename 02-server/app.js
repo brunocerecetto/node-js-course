f79588be4048262f1f8bd15a1727b1aaceb06326
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-08-29 10:42:21 -0300

    :construction: update response
    return different response according to the url

--- 02-server/app.js
+++ 02-server/app.js
@@ -2,7 +2,17 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+
   res.setHeader('Content-Type', 'text/html');
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
 
   res.write('<html>');
   res.write('<head><title>My first page</title></head>');
