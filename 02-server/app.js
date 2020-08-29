f695e4e40efc7b1b2a632be8e9f32950786c9f05
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-08-29 10:31:34 -0300

    :construction: adding response

--- 02-server/app.js
+++ 02-server/app.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><h1>Hello from my node.js server!</h1></body>');
+  res.write('</html>');
+  res.end();
+
 });
 
 server.listen(3000)
