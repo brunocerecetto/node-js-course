2dd56586ed6f141a63ce914a6e7d4a20ecc8c5ff
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-09-02 19:35:43 -0300

    :art: adding path helper

--- 03-express-js/app.js
+++ 03-express-js/app.js
@@ -1,19 +1,23 @@
+// node modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+//routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const app = express();
+//helpers
+const rootDir = require('./helpers/path');
 
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(3000);
