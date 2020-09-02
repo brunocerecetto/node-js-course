13e6eb8a447c15ac3f04267a1eaf08e88563f26a
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-09-02 19:24:33 -0300

    :lipstick: serving html pages

--- 03-express-js/app.js
+++ 03-express-js/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -8,11 +9,12 @@ const app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('admin', adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
+
