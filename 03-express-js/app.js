9cf53ff5eec179afe8f4056d359fcfc38007d719
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-09-02 20:00:37 -0300

    :wrench: adding middleware for static file serve

--- 03-express-js/app.js
+++ 03-express-js/app.js
@@ -7,17 +7,16 @@ const path = require('path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-//helpers
-const rootDir = require('./helpers/path');
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000);
