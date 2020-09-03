ca8a8dca946dbf9e759f994ca1896f8205c928db
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-09-03 19:43:20 -0300

    :sparkles: saving products into a file

--- 05-mvc/models/product.js
+++ 05-mvc/models/product.js
@@ -1,15 +1,35 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const pathUtil = require('../util/path');
+
+const filePath = path.join(pathUtil, 'data', 'products.json');
 
 module.exports = class Product {
+
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this);
+    fs.readFile(filePath, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      };
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    fs.readFile(filePath, (err, fileContent) => {
+      if (!err) {
+        callback([]);
+      };
+      callback(JSON.parse(fileContent));
+    })
   }
 }
