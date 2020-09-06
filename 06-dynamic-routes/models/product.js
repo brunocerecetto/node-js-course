5945186a74e15e5fa8de092f91fcc79d58033f2d
Bruno Cerecetto <bruno.cerecetto@gmail.com>
2020-09-06 17:56:25 -0300

    :recycle: adding code from course

--- 06-dynamic-routes/models/product.js
+++ 06-dynamic-routes/models/product.js
@@ -1,34 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathUtil = require('../util/path');
-const filePath = path.join(pathUtil, 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (callback) {
-  fs.readFile(filePath, (err, fileContent) => {
-    if (!err) {
-      return callback([]);
-    };
-    callback(JSON.parse(fileContent));
-  })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
-    })
+    });
   }
 
-  static fetchAll(callback) {
-    getProductsFromFile(callback);
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
-}
+};
